1864c91a0262cbb249cd8f347b4fd92ab523139f
antropez <spaceraiders@tutanota.com>
2021-11-15 13:56:40 +0000

    tests: finished queue init tests

--- data-structures/function/queue/tests/common.h
+++ data-structures/function/queue/tests/common.h
@@ -2,6 +2,7 @@
 #define CT_QUEUE_COMMON_H
 
 #include <string.h>
+#include <assert.h>
 
 #include "../queue.h"
 
@@ -12,5 +13,6 @@ static void free_string(const char *string) {
 ct_queue_define_node(StringNode, char*);
 ct_queue_define_queue(StringQueue, StringNode);
 static ct_queue_define_init(StringQueue, StringNode, string)
+static ct_queue_define_free(StringQueue, StringNode, string, free_string)
 
 #endif
--- data-structures/function/queue/tests/init.c
+++ data-structures/function/queue/tests/init.c
@@ -2,4 +2,10 @@
 
 int main() {
     struct StringQueue *queue = string_queue_init();    
+
+    assert(queue->front == NULL);
+    assert(queue->back == NULL);
+    assert(queue->length == 0);
+
+    string_queue_free(queue);
 }
